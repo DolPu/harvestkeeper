96c2a247d28a4f4ac1c260c493292551af115e48
Dong Phung <dolphung0708@gmail.com>
2023-02-27 19:03:33 +0700

    ! player battle controller : hitable component ref

--- Assets/Team/Scripts/Battle/Player/PlayerBattleController.cs
+++ Assets/Team/Scripts/Battle/Player/PlayerBattleController.cs
@@ -6,10 +6,18 @@ using UnityEngine;
 public class PlayerBattleController : MonoBehaviour
 {
     private PlayerBattleData currentPlayerBattleData;
-
+    public HitableComponent hitableComponent;
     public void Init(PlayerBattleData data)
     {
         currentPlayerBattleData = data;
         // logic
+        hitableComponent.Init(currentPlayerBattleData.MaxHP);
+    }
+    
+    
+    // Editor Code
+    void OnValidate()
+    {
+        hitableComponent = GetComponent<HitableComponent>();
     }
 }
